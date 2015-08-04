90b799b0ecf4426df7ef6d2552f0395b62d0363f
Israel Santana <isra@miscorreos.org>
2015-08-04 01:58:51 +0100

    refactor

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,10 +6,19 @@ class FizzBuzz(object):
     self.number = number
 
   def parse(self):
-    if self.number == 6:
-      return '1\nFizz\nBuzz\nFizz\n5\nFizzBuzz\n'
-    if self.number == 3:
-      return '1\nFizz\nBuzz\n'
-    if self.number == 2:
-      return '1\nFizz\n'
-    return '%i\n' % self.number
+    result = ''
+    for i in range(1, self.number+1):
+      result += self.parse_number(i)
+    return result
+
+
+  def parse_number(self, number):
+    result = ''
+    if number % 2 == 0:
+      result = 'Fizz'
+    if number % 3 == 0:
+      result += 'Buzz'
+    if result == '':
+      result = '%s' % str(number)
+
+    return result + '\n'
